627e2e33c583a9608edae665061d9a2d6849e8f9
zaferkrk <zafer.kirik@net-bt.com.tr>
2024-02-25 16:54:03 +0300

    test

--- API/Program.cs
+++ API/Program.cs
@@ -11,7 +11,7 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers().AddJsonOptions(opts =>
 {
     var enumConverter = new JsonStringEnumConverter();
-    opts.JsonSerializerOptions.Converters.Add(enumConverter);
+    opts.JsonSerializerOptions.Converters.Add(enumConverter); 
 });
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
